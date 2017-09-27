7070e0dea23ad71d9fe90d26f93aa71bfb74a837
jjc62351 <jjc62351@diamond.ac.uk>
2017-09-27 12:10:53 +0000

    Debugging PV and write enable PVs added

--- laserPuckPointerApp/src/iSpyBWrite.c
+++ laserPuckPointerApp/src/iSpyBWrite.c
@@ -34,13 +34,25 @@ static long isbWrite( aSubRecord *prec) {
     char pythonPath[] = "/dls_sw/apps/python/anaconda/1.7.0/64/bin/python";
 
     unsigned short initialised = *(unsigned short *)prec->c;
+    unsigned short writeEnable = *(unsigned short *)prec->f;
+    unsigned short debugEnable = *(unsigned short *)prec->g;
     if(initialised > 0){
         strcpy(dom,prec->a);
         position = *(unsigned short*)prec->b;
         strcpy(barcode,prec->d);
 
         sprintf(scriptCommand,"%s %s %s %s %d",pythonPath,scriptPath,dom,barcode,position);
-        system(scriptCommand);
+        if(debugEnable == 1){
+            if(writeEnable == 1){
+                printf("Ran: %s\n",scriptCommand);
+            }else{
+                printf("Would have ran (write currently disabled): %s\n",scriptCommand);
+            }           
+            printf("Write enabled : %d\n",writeEnable);
+        }
+        if(writeEnable == 1){
+            system(scriptCommand);
+        }
     }
     *(unsigned short*)prec->vala = 1;
     return(0);
