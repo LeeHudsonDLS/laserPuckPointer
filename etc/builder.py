350d8e78e9a5665f3d93224ad1b1fbedbdad38ea
jjc62351 <jjc62351@diamond.ac.uk>
2018-05-30 14:22:26 +0000

    First commit of builder support

--- etc/builder.py
+++ etc/builder.py
@@ -1,2 +1,27 @@
 
+import subprocess
+from iocbuilder import Device, AutoSubstitution, Xml
+from iocbuilder.modules.asyn import AsynIP
+from iocbuilder.modules.streamDevice import AutoProtocol
+from iocbuilder.arginfo import *
+
+class laserPuckPointerLib(Device):
+    LibFileList = ['laserPuckPointer']
+    DbdFileList = ['laserPuckPointer']
+    AutoInstantiate = True
+
+class laserPuckPointer(AutoSubstitution, AutoProtocol ):
+    ## Parse this template file for macros
+    TemplateFile = 'laserPuckPointer.db'
+    ProtocolFiles = ['laserPuckPointer.proto']
+    Dependencies = (laserPuckPointerLib, AsynIP  )
+    scanList = ['.1 second','.2 second','.5 second','1 second','2 second','5 second','10 second','I/O Intr','Event','Passive']
+    ArgInfo = makeArgInfo(
+        P = Simple("Device Prefix", str),
+        PORT = Ident("Asyn Port", AsynIP),
+        DEMAND_IN = Simple("Demand position from PLC", str),
+        SCAN = Choice("Scan time", scanList),
+        ROBOT = Simple("Robot prefix", str),
+        BL = Simple("Beamline prefix to run iSpyB write script", str),
+    )
 
